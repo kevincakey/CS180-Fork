3f89a1065cbe7fcbffe7c255e33dc902ce2d113d
Nick Santini <Nicholassantini32@gmail.com>
2023-02-01 17:07:45 -0800

    Adds react framework

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component }  from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Form, Dropdown, Collapse, Col, Row} from 'react-bootstrap';
+
+
+//TODO: DEFINE DATABASE ELEMENTS GLOBALLY
+
+//homepage display class
+export default class Page extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+
+  render(){
+    return(
+      <div>
+
+      </div>
+    );
+  }
+
+
+}
