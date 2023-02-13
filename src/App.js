306fd2b02be289ce5502f9bd451db60321b25ab5
Anchita Bora <anchita.bora@gmail.com>
2023-02-12 23:59:20 -0800

    moved navbar to the right

--- src/App.js
+++ src/App.js
@@ -17,12 +17,12 @@ export default class Page extends React.Component {
     return(
       <div>
        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="#">Fork</a>
+          <a class="navbar-brand justify-content-end" href="#">Fork</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
+            <ul class="navbar-nav ms-auto">
               <li class="nav-item active">
                 <a class="nav-link" href="#">Home</a>
               </li>
