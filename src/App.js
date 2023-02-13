a015511b6f1dc4546d802ba4a8c8eaa96fd049f2
Anchita Bora <anchita.bora@gmail.com>
2023-02-12 23:35:57 -0800

    added brand name in navbar

--- src/App.js
+++ src/App.js
@@ -16,7 +16,15 @@ export default class Page extends React.Component {
   render(){
     return(
       <div>
-       
+       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">Fork</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          
+        </nav>
+
         <h1> Fork Landing Page</h1>
         <ul>
           <li> How It Works </li>
