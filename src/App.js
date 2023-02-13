ba67fb045aeb6114e869286068259ef4ec8de94b
Anchita Bora <anchita.bora@gmail.com>
2023-02-13 00:10:26 -0800

    added bg color

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ export default class Page extends React.Component {
 
   render(){
     return(
-      <div>
-       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div style={{backgroundColor: "#e7f8ff"}}>
+       <nav class="navbar navbar-expand-lg navbar-light bg-light" >
           <a class="navbar-brand justify-content-end" href="#">Fork</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
