f3ec3e8675daf3f26020eeed4d50de932bf3bb6f
kevincakey <okevin.gao@gmail.com>
2023-02-22 17:37:52 -0800

    Person to person successful

--- src/App.js
+++ src/App.js
@@ -1,34 +1,90 @@
-import React, { Component }  from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Form, Dropdown, Collapse, Col, Row} from 'react-bootstrap';
-import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import { useEffect } from "react";
+import {
+    PayPalScriptProvider,
+    PayPalButtons,
+    usePayPalScriptReducer
+} from "@paypal/react-paypal-js";
 
-//homepage display class
-export default class Page extends React.Component {
-  constructor(props){
-    super(props);
-  }
+// This values are the props in the UI
+const amount = "56";
+const currency = "USD";
+const style = {"layout":"vertical"};
+
+// Custom component to wrap the PayPalButtons and handle currency changes
+const ButtonWrapper = ({ currency, showSpinner }) => {
+    // usePayPalScriptReducer can be use only inside children of PayPalScriptProviders
+    // This is the main reason to wrap the PayPalButtons in a new component
+    const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
+
+    useEffect(() => {
+        dispatch({
+            type: "resetOptions",
+            value: {
+                ...options,
+                currency: currency,
+            },
+        });
+    }, [currency, showSpinner]);
 
 
-  render(){
-    return(
-      <div>
-        <Payment/>
-      </div>
+    return (<>
+            { (showSpinner && isPending) && <div className="spinner" /> }
+            <PayPalButtons
+                style={style}
+                disabled={false}
+                forceReRender={[amount, currency, style]}
+                fundingSource={undefined}
+                createOrder={(data, actions) => {
+                    let order =actions.order
+                        .create({
+                            purchase_units: [
+                                {
+                                    amount: {
+                                        currency_code: currency,
+                                        value: amount,
+                                    },
+                                    payee: {
+                                        email_address: "sb-uhvss25066906@personal.example.com"
+                                    }                                     
+                                }
+                            ]
+                        })
+
+                    // order.payee.email = "sb-uhvss25066906@personal.example.com";
+                    console.log(order);
+
+                    order.then((orderId) => {
+                        // Your code here after create the order
+                        return orderId;
+                    });
+                    
+                    return order;
+                }}
+                onApprove={function (data, actions) {
+                    return actions.order.capture().then(function () {
+                        // Your code here after capture the order
+                    });
+                }}
+            />
+        </>
     );
-  }
-
-
 }
 
-class Payment extends React.Component {
-
-
-  render(){
-    return (
-        <PayPalScriptProvider options={{ "client-id": "test" }}>
-            <PayPalButtons style={{ layout: "vertical" }} />
-        </PayPalScriptProvider>
-    );
-  }
+export default function App() {
+	return (
+		<div style={{ maxWidth: "750px", minHeight: "200px" }}>
+            <PayPalScriptProvider
+                options={{
+                    "client-id": "test",
+                    components: "buttons",
+                    currency: "USD"
+                }}
+            >
+				<ButtonWrapper
+                    currency={currency}
+                    showSpinner={false}
+                />
+			</PayPalScriptProvider>
+		</div>
+	);
 }
