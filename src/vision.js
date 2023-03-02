e5452a6ce04c1efca3f39d118320852b55498684
Garrrrrrrrry <zeng.gary0@gmail.com>
2023-03-01 16:22:04 -0800

    add result to text parsing

--- src/vision.js
+++ src/vision.js
@@ -1,26 +1,46 @@
 import { useState } from 'react';
 import Tesseract from 'tesseract.js';
 
+function parseResultToBill(result){
+  //var text = result.data.text + "";
+  var lines = result.data.lines, size = lines.length;
+  var bill = "", index = 0;
+  while(index < size){
+    let currentLine = lines[index].text;
+    var charBeforeNL = currentLine.charAt((currentLine.indexOf('\n')) - 1);
+    if(currentLine.indexOf("otal") !== -1){
+      bill += currentLine;
+      return bill;
+    }
+    if(!(isNaN(charBeforeNL))){
+      bill += currentLine;
+    }
+    ++index;
+  }
+  return bill;
+}
+
 export default function Vision() {
   const [imagePath, setImagePath] = useState("");
- 
+  
   const handleChange = (event) => {
     setImagePath(URL.createObjectURL(event.target.files[0]));
   }
- 
+  //====problem of tesseract having slightly higher confidence scores on decimal numbers that are incorrect====
   const handleClick = () => {
-  
     Tesseract.recognize(imagePath,'eng', 
       { logger: m => console.log(m) })
     .catch (err => {
       console.error(err) })
     .then(result => {
-      console.log(result) })
+      var bill = parseResultToBill(result);
+      console.log(result);
+      console.log(bill);
+    })
   }
- 
   return (
     <div className="App">
-      <main className="App-main">
+      <main className="App-mainD">
         <h3>Actual imagePath uploaded</h3>
         <img 
            src={imagePath} className="App-image" alt="logo"/>
