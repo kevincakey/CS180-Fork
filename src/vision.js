3ec16813a77797665c2d51a5a7234853d5a2c447
caleb <91437024+c9leb@users.noreply.github.com>
2023-03-04 14:58:14 -0800

    added binarize functions to preprocess the image before OCR is done

--- src/vision.js
+++ src/vision.js
@@ -1,23 +1,46 @@
 import { useState } from 'react';
 import Tesseract from 'tesseract.js';
 
-function parseResultToBill(result){
-  //var text = result.data.text + "";
-  var lines = result.data.lines, size = lines.length;
-  var bill = "", index = 0;
-  while(index < size){
-    let currentLine = lines[index].text;
-    var charBeforeNL = currentLine.charAt((currentLine.indexOf('\n')) - 1);
-    if(currentLine.indexOf("otal") !== -1){
-      bill += currentLine;
-      return bill;
-    }
-    if(!(isNaN(charBeforeNL))){
-      bill += currentLine;
-    }
-    ++index;
+
+//test different threshold values used ideas from https://dev.to/mathewthe2/using-javascript-to-preprocess-images-for-ocr-1jc
+function binarize(image, threshold) {
+  
+  for(let i = 0; i < image.data.length; i += 4) {
+    const r = image.data[i];
+    const g = image.data[i + 1];
+    const b = image.data[i + 2];
+
+    let average = ((r + g + b) / 3 ) //calculates average rgb value for each pixel in the image
+    let value = average < threshold ? 0 : 255;
+    image.data[i] = image.data[i+1] = image.data[i+2] = value;
   }
-  return bill;
+
+  return image;
+
+  
+}
+
+function binarizePath(imagePath, threshold, callback) {
+  var img = new Image();
+  img.onload = function() {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    binarize(imageData, threshold);
+
+    ctx.putImageData(imageData, 0, 0);
+
+    var binaryImageDataUrl = canvas.toDataURL(); // get the binary image as a data URL
+
+    callback(binaryImageDataUrl);
+
+  }
+  img.src = imagePath;
 }
 
 export default function Vision() {
@@ -26,18 +49,19 @@ export default function Vision() {
   const handleChange = (event) => {
     setImagePath(URL.createObjectURL(event.target.files[0]));
   }
+
+
   //====problem of tesseract having slightly higher confidence scores on decimal numbers that are incorrect====
   const handleClick = () => {
-    Tesseract.recognize(imagePath,'eng', 
-      { logger: m => console.log(m) })
-    .catch (err => {
-      console.error(err) })
-    .then(result => {
-      var bill = parseResultToBill(result);
-      console.log(result);
-      console.log(bill);
-    })
-  }
+    binarizePath(imagePath, 100, (binaryImageDataUrl) => {
+    Tesseract.recognize(binaryImageDataUrl,'eng', 
+    { logger: m => console.log(m) })
+  .catch (err => {
+    console.error(err) })
+  .then(result => {
+    console.log(result) })
+  });
+}
   return (
     <div className="App">
       <main className="App-mainD">
