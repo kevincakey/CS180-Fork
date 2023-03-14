e390c2d75cdbee26d6b135e96e802dc6ffa7fd9a
Anchita Bora <anchita.bora@gmail.com>
2023-03-14 12:17:35 -0700

    added works page styling & scan info

--- src/works.js
+++ src/works.js
@@ -1,11 +1,15 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './works.css'
 
 const Works = () => {
 return (
-	<div>
-	<h1>
-		How fork works ... 
-	</h1>
+	<div class="me-5 main-div body">
+		<h1> How Fork Works ... </h1>
+		Scan
+		<p> We use Tesseract.js, an optical character recognition engine to parse your receipt into readable json for our code to understand what items need to be split and how much they were.
+</p>
+
 	</div>
 );
 };
